0ec55cff890c840afa374c7266da9567496e333e
Raging Beardo <beardos.services@gmail.com>
2025-02-24 00:30:32 -0500

    safety check

--- src/mod.ts
+++ src/mod.ts
@@ -30,6 +30,13 @@ class Mod implements IPostDBLoadMod {
         const tables: IDatabaseTables = databaseServer.getTables();
         const locations = tables.locations;
 
+        const defaultAirdropIcons = {
+            mixed: "Common",
+            barter: "Supply",
+            foodMedical: "Medical",
+            weaponArmor: "Weapon"
+        };
+
         const ignoredLocations = [
             "develop",
             "factory4_day",
@@ -60,7 +67,12 @@ class Mod implements IPostDBLoadMod {
                 if (this.modConfig.debugLogsEnabled) {
                     logger.logWithColor(`airdropconfig: Setting airdrop type weight for ${SptAirdropTypeEnum[type]} to ${this.modConfig.airdropTypeWeighting[SptAirdropTypeEnum[type]]}`, LogTextColor.YELLOW);
                 }
-                airdropConfig.loot[SptAirdropTypeEnum[type]] = this.modConfig.airdropLootConfig[SptAirdropTypeEnum[type]];
+
+                if (defaultAirdropIcons[SptAirdropTypeEnum[type]] == this.modConfig.airdropLootConfig[SptAirdropTypeEnum[type]].icon) {
+                    airdropConfig.loot[SptAirdropTypeEnum[type]] = this.modConfig.airdropLootConfig[SptAirdropTypeEnum[type]];
+                } else {
+                    logger.logWithColor(`Loot settings not applied for ${SptAirdropTypeEnum[type]}. Ensure icon value is ${defaultAirdropIcons[SptAirdropTypeEnum[type]]}`, LogTextColor.RED);
+                }
             }
         }
 
@@ -82,7 +94,6 @@ class Mod implements IPostDBLoadMod {
                     if (this.modConfig.debugLogsEnabled) {
                         logger.logWithColor(`airdropconfig: Setting airdrop start max for ${location} to ${this.modConfig.airdropTiming["planeAirdropStartMax"]}`, LogTextColor.YELLOW);
                     }
-                    
                 }
             }
         }
