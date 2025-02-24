2998bf5c94660e48db6ad75a806349bd1e1cf9b6
Raging Beardo <beardos.services@gmail.com>
2025-02-23 23:37:08 -0500

    config update

--- src/model/config.ts
+++ src/model/config.ts
@@ -1,6 +1,7 @@
 import { IAirdropLoot } from "@spt/models/spt/config/IAirdropConfig";
 
 interface Config {
+    debugLogsEnabled: boolean;
     airdropTiming: Record<string, number>;
     airdropTypeWeighting: Record<string, number>;
     airdropPercentChanceByLocation: Record<string, number>;
