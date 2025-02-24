963a4adf144b62f5e30e795a1a1020bada19d6ad
Raging Beardo <beardos.services@gmail.com>
2025-02-23 22:29:16 -0500

    init

---
+++ src/mod.ts
@@ -0,0 +1,74 @@
+/* eslint-disable @typescript-eslint/brace-style */
+import path from "node:path";
+import { DependencyContainer } from "tsyringe";
+
+import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
+import { DatabaseServer } from "@spt/servers/DatabaseServer";
+import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
+import { ConfigServer } from "@spt/servers/ConfigServer";
+import { IAirdropConfig } from "@spt/models/spt/config/IAirdropConfig";
+import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
+import { VFS } from "@spt/utils/VFS";
+import { SptAirdropTypeEnum } from "@spt/models/enums/AirdropType";
+
+import JSON5 from "json5";
+import config from "./model/config";
+
+class Mod implements IPostDBLoadMod {
+    private modConfig: config;
+
+    public postDBLoad(container: DependencyContainer): void {
+        const vfs = container.resolve<VFS>("VFS");
+        this.modConfig = JSON5.parse(vfs.readFile(path.resolve(__dirname, "../config/config.json5")));
+
+        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const configServer = container.resolve<ConfigServer>("ConfigServer");
+        const airdropConfig = configServer.getConfig<IAirdropConfig>(ConfigTypes.AIRDROP);
+        const tables: IDatabaseTables = databaseServer.getTables();
+        const locations = tables.locations;
+
+        const ignoredLocations = [
+            "develop",
+            "factory4_day",
+            "factory4_night",
+            "hideout",
+            "laboratory",
+            "privatearea",
+            "suburbs",
+            "terminal",
+            "town",
+            "base"
+        ];
+
+        const locationMapping = {
+            customs: "bigmap",
+            interchange: "interchange",
+            woods: "woods",
+            shoreline: "shoreline",
+            reserve: "rezervbase",
+            groundzero: ["sandbox", "sandbox_high"],
+            streets: "tarkovstreets",
+            lighthouse: "lighthouse"
+        };
+
+        for (const type in SptAirdropTypeEnum) {
+            if (SptAirdropTypeEnum[type] !== SptAirdropTypeEnum.RADAR) {
+                airdropConfig.airdropTypeWeightings[SptAirdropTypeEnum[type]] = this.modConfig.airdropTypeWeightings[SptAirdropTypeEnum[type]];
+                airdropConfig.loot[SptAirdropTypeEnum[type]] = this.modConfig.airdropLootConfig[SptAirdropTypeEnum[type]];
+            }
+        }
+
+        for (const configLocation in locationMapping) {
+            const mappedLocations = locationMapping[configLocation];
+            const locationsArray = Array.isArray(mappedLocations) ? mappedLocations : [mappedLocations];
+
+            for (const location of locationsArray) {
+                if (!ignoredLocations.includes(location)) {
+                    locations[location].base.AirdropParameters[0].PlaneAirdropChance = parseFloat((this.modConfig.airdropPercentChanceByLocation[configLocation] / 100).toFixed(2));
+                }
+            }
+        }
+    }
+}
+
+export const mod = new Mod();
---
+++ src/model/config.ts
@@ -0,0 +1,9 @@
+import { IAirdropLoot } from "@spt/models/spt/config/IAirdropConfig";
+
+interface Config {
+    airdropTypeWeightings: Record<string, number>;
+    airdropPercentChanceByLocation: Record<string, number>;
+    airdropLootConfig: Record<string, IAirdropLoot>;
+}
+
+export default Config;
