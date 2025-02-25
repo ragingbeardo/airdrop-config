74fbdeeedcd1e66acd5f77f82352302b4952011b
Raging Beardo <beardos.services@gmail.com>
2025-02-24 23:57:53 -0500

    update list logic

--- src/mod.ts
+++ src/mod.ts
@@ -7,14 +7,14 @@ import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
 import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
 import { ConfigServer } from "@spt/servers/ConfigServer";
-import { IAirdropConfig } from "@spt/models/spt/config/IAirdropConfig";
+import { IAirdropConfig, IAirdropLoot } from "@spt/models/spt/config/IAirdropConfig";
 import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
 import { VFS } from "@spt/utils/VFS";
 import { SptAirdropTypeEnum } from "@spt/models/enums/AirdropType";
+import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
 
 import JSON5 from "json5";
 import config from "./model/config";
-import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
 
 class Mod implements IPostDBLoadMod {
     private modConfig: config;
@@ -61,6 +61,56 @@ class Mod implements IPostDBLoadMod {
             lighthouse: "lighthouse"
         };
 
+        const lootConfigListsToAmend = [
+            "itemBlacklist",
+            "itemTypeWhitelist",
+            "itemLimits",
+            "itemStackLimits",
+            "armorLevelWhitelist"
+        ];
+
+        const combineListsFunctions = {
+            itemBlacklist: combineBasicList,
+            itemTypeWhitelist: combineBasicList,
+            itemLimits: combineBasicKVPairs,
+            itemStackLimits: combineBasicKVPairs,
+            armorLevelWhitelist: combineBasicList
+        };
+
+        function combineBasicList(baseList: any[], modList: any[]): any[] {
+            const combinedList = new Set(baseList);
+            for (const item of modList) {
+                combinedList.add(item);
+            }
+            const resultList = Array.from(combinedList);
+            if (resultList.every(item => typeof item === "number")) {
+                resultList.sort((a, b) => a - b);
+            }
+            return resultList;
+        }
+
+        function combineBasicKVPairs(baseObj: Record<string, any>, modObj: Record<string, any>): Record<string, any> {
+            const combinedObj = { ...baseObj };
+            for (const key in modObj) {
+                combinedObj[key] = modObj[key];
+            }
+            return combinedObj;
+        }
+
+        function mergeLootConfiguration(baseLootConfigForType: IAirdropLoot, modLootConfigForType: IAirdropLoot) {
+            for (const field in modLootConfigForType) {
+                if (field !== "icon") {
+                    if (lootConfigListsToAmend.includes(field)) {
+                        let baseLootConfig: IAirdropLoot = baseLootConfigForType[field];
+                        const modLootConfig: IAirdropLoot = modLootConfigForType[field];
+                        baseLootConfig = combineListsFunctions[field](baseLootConfig, modLootConfig);
+                    } else {
+                        baseLootConfigForType[field] = modLootConfigForType[field];
+                    }
+                }
+            }
+        }
+
         if (this.modConfig.toggle.airDropTypeWeightingEnabled || this.modConfig.toggle.airDropLootConfigEnabled) {
             for (const type in SptAirdropTypeEnum) {
                 if (SptAirdropTypeEnum[type] !== SptAirdropTypeEnum.RADAR) {
@@ -72,11 +122,12 @@ class Mod implements IPostDBLoadMod {
                     }
 
                     if (this.modConfig.toggle.airDropLootConfigEnabled) {
-                        if (defaultAirdropIcons[SptAirdropTypeEnum[type]] == this.modConfig.airdropLootConfig[SptAirdropTypeEnum[type]].icon) {
+                        if (this.modConfig.toggle.replaceLists) {
                             this.modConfig.airdropLootConfig[SptAirdropTypeEnum[type]].icon = defaultAirdropIcons[SptAirdropTypeEnum[type]];
                             airdropConfig.loot[SptAirdropTypeEnum[type]] = this.modConfig.airdropLootConfig[SptAirdropTypeEnum[type]];
                         } else {
-                            logger.logWithColor(`airdropconfig: Loot settings not applied for ${SptAirdropTypeEnum[type]}. Ensure icon value is ${defaultAirdropIcons[SptAirdropTypeEnum[type]]}`, LogTextColor.RED);
+                            logger.logWithColor(`airdropconfig: Merging loot configuration for ${SptAirdropTypeEnum[type]}`, LogTextColor.YELLOW);
+                            mergeLootConfiguration(airdropConfig.loot[SptAirdropTypeEnum[type]], this.modConfig.airdropLootConfig[SptAirdropTypeEnum[type]]);
                         }
                     }
                 }
